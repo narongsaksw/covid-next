d857028ceef0e362e5cb29d01ad346f77ee05d79
sscarry2 <earth.narongsak@gmail.com>
2021-12-15 14:54:01 +0700

    add type

---
+++ type/type.ts
@@ -0,0 +1,52 @@
+import * as yup from 'yup'
+export type FormData = {
+    citizenId: string;
+    gender: string;
+    age: number;
+    occupation: string;
+    from: string;
+    to: string;
+    detail: string;
+    locationType: string;
+    locationName: string;
+};
+
+export enum LOCATION {
+    INDOOR = 'INDOOR',
+    OUTDOOR = 'OUTDOOR',
+    HOME = 'HOME',
+    TRAVELLING = 'TRAVELLING'
+}
+export enum GENDER {
+    MALE = 'MALE',
+    FEMALE = 'FEMALE',
+}
+
+export const formSchema = yup.object({
+    citizenId: yup.string().required('Citizen ID is required.'),
+    gender: yup.string().required('Gender is required.'),
+    age: yup.number().positive('Age should be more than 0').integer().typeError('Age is required.'),
+    occupation: yup.string().required('Occupation is required.'),
+    from: yup.string().required('From Date is required.'),
+    to: yup.string().required('To Date is required.'),
+    detail: yup.string().required('Detail is required.'),
+    locationType: yup.string().required('Location Type is required.'),
+    locationName: yup.string().required('Location Name is required.'),
+})
+
+export interface Timeline {
+    from: string;
+    to: string;
+    detail: string;
+    locationType: string;
+    locationName: string;
+    
+} 
+
+export interface User {
+    citizenId: string;
+    gender: string;
+    age: number;
+    occupation: string;
+    timelines: Timeline[]
+}
